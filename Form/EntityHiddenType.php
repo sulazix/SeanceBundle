b7eabfa061395b79cc5f36df642a1f357c7e56df
Florian Alonso <florian.alonso@epfl.ch>
2015-10-20 14:59:20 +0200

    Added custom entity hidden field

---
+++ Form/EntityHiddenType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Interne\SeanceBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+use Interne\SeanceBundle\Transformer\EntityToIdTransformer;
+
+class EntityHiddenType extends AbstractType
+{
+    /**
+     * @var ObjectManager
+     */
+    protected $manager;
+
+    public function __construct(ObjectManager $manager)
+    {
+        $this->manager = $manager;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $transformer = new EntityToIdTransformer($this->manager, $options['class']);
+        $builder->addModelTransformer($transformer);
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver
+            ->setRequired(array('class'))
+            ->setDefaults(array(
+                'invalid_message' => 'The entity does not exist.',
+            ))
+        ;
+    }
+
+    public function getParent()
+    {
+        return 'hidden';
+    }
+
+    public function getName()
+    {
+        return 'entity_hidden';
+    }
+}
