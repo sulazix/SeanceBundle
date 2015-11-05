295a837c566af9a2912d11096c3d3b607c19c44f
Florian Alonso <florian.alonso@epfl.ch>
2015-11-05 19:08:56 +0100

    Search field is now functional :
    Keywords will be looked for in :
    - meeting name
    - meeting date
    - meeting place
    
    The search mecanism could probably be improved for date filtering but for now it works just fine :)

---
+++ Resources/public/js/filters/SearchFilter.js
@@ -0,0 +1,25 @@
+seanceApp.filter('searchFilter', function() {
+  return function (objects, query, properties) {
+
+    if (!query || query == "") return objects;
+
+    var filtered_list = [];
+
+    angular.forEach(objects, function(value, key){
+       
+       var add = false;
+       angular.forEach(properties, function(property, key) {
+        // Try value until a match is found
+        if (!add)
+          add = angular.lowercase(value[property]).indexOf(angular.lowercase(query)) != -1
+       })
+      
+
+      if (add) {
+        filtered_list.push(value);
+      }
+
+    });
+    return filtered_list;
+  }
+});
